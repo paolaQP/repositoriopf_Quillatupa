55f82cb3c4414eef3bfde750f5d06549e63c9cf2
Vanessa <Vanessa@DESKTOP-754GRP1>
2025-02-16 02:13:06 -0500

    Final Proyect

---
+++ src/Programa/Programa03.java
@@ -0,0 +1,8 @@
+
+package Programa;
+
+public class Programa03 {
+    public static void main (String args[]){
+        System.out.println("Configurar netbeans para vincularlo con el repositorio de GitHub");
+    }
+}
